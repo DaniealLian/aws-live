94cab99ec50e459548925eda9f78d75e5bfb3d5a
DaniealLian <151511915+DaniealLian@users.noreply.github.com>
2025-11-29 13:20:23 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.cjs7jydjb57u.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "admin123"
+customdb = "employee"
+custombucket = "danieal-employee"
+customregion = "us-east-1"
+
